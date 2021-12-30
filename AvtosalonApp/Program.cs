ef70bdc722383513168f0ae4542358014df973ee
Satoru78 <viewgold931@gmail.com>
2021-12-30 16:25:37 +0300

    sE

---
+++ AvtosalonApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AvtosalonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!");
+        }bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
